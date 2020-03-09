a30c0fbb5c8f9a7be4352d378a93ca0b8625d148
r-behrends <61775290+r-behrends@users.noreply.github.com>
2020-03-09 08:20:46 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -5,14 +5,7 @@ public class main
     
      //ab hier schreibt ihr euren Quelltext
     
-    int x = 1;
-    int zaehler=0;
-
-        while( x<1000000 ){
-            x = x*2;      // x wird hier verdoppelt
-            zaehler = zaehler + 1;
-            
-        }
-    System.out.println("x muss so oft verdoppelt werden: " + zaehler);
+    Kreis k1 = new Kreis();
+    Rechteck r1 = new Rechteck();
     }    
 }
