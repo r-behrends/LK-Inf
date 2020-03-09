c691bcf4de106f582c7b3e365af9bae67bd7b175
r-behrends <61775290+r-behrends@users.noreply.github.com>
2020-03-09 09:08:05 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,7 +4,7 @@ public class main
     public static void main(String[] args){
     
      //ab hier schreibt ihr euren Quelltext
-    
+    //Neues Kommentar
     Kreis k1 = new Kreis();
     Rechteck r1 = new Rechteck();
     }    
